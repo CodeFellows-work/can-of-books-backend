fb4538ca419f01d31c2f9406dd709aad79c1cbf3
sunny-lee3 <sunnylee3712@gmail.com>
2021-05-24 16:36:41 -0700

    updated lab 11 backend

--- server.js
+++ server.js
@@ -10,11 +10,27 @@ const jwksClient = require('jwks-rsa');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
+// Follows along with Lab tutorial 
+const client = jwksClient ({
+    jwksUri: 'http://dev-nt37xvb0.us.auth0.com/.well-known/jwks.json'
+})
+function getKey(header, callback) {
+    client.getSigningKey(header.kid, function(err, key) {
+        const signInKey = key.publicKey || key.rsaPublicKey; 
+        callback(null,signInKey); 
+    } )
+}
 
-
-app.get('/test', (request, response) => {
+app.get('/user', (request, response) => {
+    const token = request.headers.authorization.split(' ')[1]; 
+    jwt.verify(token, getKey, {}, (err, user) => {
+        if(err){ 
+            response.send("Uh Oh... Something Went Wrong"); 
+        }
+            response.send(user);
+    });
 
   // TODO: 
   // STEP 1: get the jwt from the headers
@@ -25,3 +41,8 @@ app.get('/test', (request, response) => {
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+//Domain
+//dev-nt37xvb0.us.auth0.com
+// ClientID 
+//xq8PHihp3RkyWMdVkjvU5hA92Wb9PrTf
